801c00e737783c1a1f36374f32091d9aeadf48e8
kittenwarrior-qb <kittenwarrior2005@gmail.com>
2026-03-03 10:07:17 +0700

    loops java

---
+++ Java/javabase/Loops.java
@@ -0,0 +1,38 @@
+// public class Loops {
+//     public static void main(String args[]) {
+//         int x = 3;
+//         while(x > 0) {
+//             System.out.println("x = " + x--);
+//         }
+
+//     }
+// }
+
+// public class Loops {
+//     public static void main(String args[]) {
+//         int x = 3;
+//         do {
+//             System.out.println("x is " + x--);
+//         } while (x < 0);
+
+//     }
+// }
+
+
+// public class Loops {
+//     public static void main(String args[]) {
+//         for (int x = 3; x > 0; x--) {
+//             System.out.println("x is " + x);
+//         }
+//     }
+// }
+
+
+public class Loops {
+    public static void main(String args[]) {
+        int[] numbers = {0, 1, 2};
+        for (int x : numbers) {
+            System.out.println(x);
+        }
+    }
+}
